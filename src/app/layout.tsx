d7804e39dea23de86c2097317500ca7e51fa462f
Cost_Victor <victorcost436@gmail.com>
2024-08-31 17:53:08 -0300

    feat: useMobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { averiaSansLibre } from "@/styles/fonts";
 import { AuthProvider } from "@/hooks/useAuth";
+import { MobileProvider } from "@/hooks/useMobile";
 import "@/styles/globals.scss";
 
 export const metadata: Metadata = {
@@ -17,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={averiaSansLibre.className}>
-        <AuthProvider>{children}</AuthProvider>
+        <AuthProvider>
+          <MobileProvider>{children}</MobileProvider>
+        </AuthProvider>
       </body>
     </html>
   );
