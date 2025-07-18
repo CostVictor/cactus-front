b7f5e234b60f44147e330619b8a75440674c4350
CostVictor <costvictor36@gmail.com>
2025-07-18 20:49:01 -0300

    feat(orders page): monitoramento dos pedidos de almoço

--- src/app/orders/lunch/page.tsx
+++ src/app/orders/lunch/page.tsx
@@ -1,12 +1,43 @@
+"use client";
+
+import Link from "next/link";
+
 import Sidebar from "@/components/navigation/Sidebar";
 import Section from "@/components/layout/Section";
+import Panel from "@/components/layout/Panel";
+import Button from "@/components/form/Button";
+
+import useWebSocket from "@/hooks/network/useWebSocket";
+import { apiWS } from "@api/endpoints";
+import { BaseOrder } from "@api/types/order";
+
+import OrdersPanel from "../_subcomponents/OrdersPanel";
 
 export default function OrdersLunchPage() {
+  const { order } = apiWS;
+  const { data } = useWebSocket<BaseOrder[]>(order.baseUrlLunch);
+
   return (
     <>
       <main>
         <Section id="ordersLunch" maxWidthContent>
-          Ok
+          <Panel title="Pedidos">
+            <div
+              style={{
+                display: "flex",
+                padding: "1rem",
+                gap: "2rem",
+              }}
+            >
+              <Link href="/orders/snack">
+                <Button text="Lanches" />
+              </Link>
+              <Button text="Almoços" appearance="principal" />
+            </div>
+          </Panel>
+
+          <p className="division space"></p>
+          {!!data && <OrdersPanel data={data} />}
         </Section>
       </main>
       <Sidebar />
